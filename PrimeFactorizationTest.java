d18f442304b9aa4cd09f605ea56d93d04b69ac05
David Galvez <galvezdm@rose-hulman.edu>
2012-03-23 17:29:43 -0400

    Initial test

---
+++ PrimeFactorizationTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class PrimeFactorizationTest {
+	@Test
+	public void test_JUnit(){
+		assertTrue("The compiler isn't feeling well today...", true);
+	}
+}
