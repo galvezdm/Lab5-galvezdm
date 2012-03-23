3e9844b95fe2066245a9f06bec8dd8c73034705f
David Galvez <galvezdm@rose-hulman.edu>
2012-03-23 17:37:26 -0400

    Test for 1 done

--- PrimeFactorizationTest.java
+++ PrimeFactorizationTest.java
@@ -1,9 +1,17 @@
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class PrimeFactorizationTest {
 	@Test
 	public void test_JUnit(){
 		assertTrue("The compiler isn't feeling well today...", true);
+	}
+	
+	@Test
+	public void testPrimeFactoringFor1(){
+		assertEqual(PrimeFactorization.getPrimeFactors(1), new ArrayList<Integer>());
 	}
 }
