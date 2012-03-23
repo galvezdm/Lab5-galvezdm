19069050e7c6a04271472fd584eaf7351b44783c
David Galvez <galvezdm@rose-hulman.edu>
2012-03-23 18:12:45 -0400

    Test up to 8 works

--- PrimeFactorization.java
+++ PrimeFactorization.java
@@ -6,8 +6,14 @@ public class PrimeFactorization {
 	public static ArrayList<Integer> getPrimeFactors(int i) {
 		
 		ArrayList<Integer> list = new ArrayList<Integer>();
-		if (i%2 == 0){
-			list.add(2);
+		
+		while(i > 0){
+			if (i%2 == 0){
+				list.add(2);
+				i /= 2;
+			}else{
+				break;
+			}
 		}
 		
 		if (i%3 == 0){
