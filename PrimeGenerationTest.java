ceb388609a3e735e8f05a7ec3f7245c1ab22991d
David Galvez <galvezdm@rose-hulman.edu>
2012-03-23 18:26:58 -0400

    Prime generation class and test class made, base test is good

---
+++ PrimeGenerationTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class PrimeGenerationTest {
+	@Test
+	public void test_JUnit(){
+		assertTrue("The compiler isn't feeling well today...", true);
+	}
+}
