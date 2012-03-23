e9b65aeab6abab6f1af643d1951fcb75f2f553de
David Galvez <galvezdm@rose-hulman.edu>
2012-03-23 18:32:53 -0400

    Prime Generation Tests for 1 and 2 work

--- PrimeGeneration.java
+++ PrimeGeneration.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
+
 
 public class PrimeGeneration {
-
+	public static ArrayList<Integer> getAllSmallerPrimes(int i) {
+		
+		return new ArrayList<Integer>();
+	}
 }
--- PrimeGenerationTest.java
+++ PrimeGenerationTest.java
@@ -1,9 +1,23 @@
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class PrimeGenerationTest {
 	@Test
 	public void test_JUnit(){
 		assertTrue("The compiler isn't feeling well today...", true);
+	}
+	
+	@Test
+	public void testPrimeGenerationFor1(){
+		assertEquals(PrimeGeneration.getAllSmallerPrimes(1), new ArrayList<Integer>());
+	}
+
+	@Test
+	public void testPrimeGenerationFor2(){
+		assertEquals(PrimeGeneration.getAllSmallerPrimes(2), new ArrayList<Integer>());
 	}
 }
